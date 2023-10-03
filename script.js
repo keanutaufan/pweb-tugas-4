e9f9111ca479f3b5ce4d65ba1ff2396bcdbd6b94
keanutaufan <keanu.taufan21@gmail.com>
2023-10-03 09:43:27 +0700

    add fetch functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+async function getWeatherData() {
+  const query = document.getElementById("search").value;
+  const res = await fetch(`https://goweather.herokuapp.com/weather/${query}`);
+  const data = await res.json();
+
+  if (res.status == 404) {
+    document.getElementById("info").style.display = "none";
+    document.getElementById("notfound").style.display = "block";
+    document.getElementById("notfound").innerText = `City '${query.replace(/^./, query[0].toUpperCase())}' not found`;
+    return;
+  }
+
+  console.log(data);
+  document.getElementById("info").style.display = "flex";
+  document.getElementById("notfound").style.display = "none";
+
+  document.getElementById("city-name").innerText = `Weather of ${query.replace(/^./, query[0].toUpperCase())}`;
+  document.getElementById("temperature").innerText = data.temperature;
+  document.getElementById("wind").innerText = data.wind;
+}
+
+window.onload = function() {
+  document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    getWeatherData();
+  });
+}
